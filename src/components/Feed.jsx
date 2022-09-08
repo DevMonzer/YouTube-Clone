72437fb2f53c698335dc62ae2c9cdbf5f180ce76
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-08 10:40:45 +0300

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import { Sidebar } from "./";
+
 const Feed = () => {
-  return <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}></Stack>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Sidebar />
+    </Stack>
+  );
 };
 
 export default Feed;
