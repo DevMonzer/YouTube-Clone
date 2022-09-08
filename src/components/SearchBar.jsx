be145242f018353943b209bb675ef61260d90376
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-08 10:28:49 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
   return <div>SearchBar</div>;
