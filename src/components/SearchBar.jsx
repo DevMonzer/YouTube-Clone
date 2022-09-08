03f2dc63abc4e076e63c14cfbb550874c5f59b29
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-08 10:30:35 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,9 +4,23 @@ import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -15,7 +29,12 @@ const SearchBar = () => {
         mr: { sm: 5 },
       }}
     >
-      <input className="search-bar" placeholder="Search..." />
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <IconButton
         type="submit"
         sx={{ p: "10px", color: "red" }}
