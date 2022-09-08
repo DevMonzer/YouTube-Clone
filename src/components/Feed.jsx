7ff14fe39b7dfc01d23b0e35ce4298e2e6f26d6a
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-08 10:43:43 +0300

    add a new component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import { Sidebar } from "./";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Sidebar />
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
     </Stack>
   );
 };
