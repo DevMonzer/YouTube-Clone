852857e32423785a11b44a48d7602f76af70c130
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-08 10:29:45 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,27 @@ import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input className="search-bar" placeholder="Search..." />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
