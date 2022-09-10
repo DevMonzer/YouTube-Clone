604f6ea479688e4b1aa3d263dbb9b581748a436a
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 18:00:47 +0300

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
   return <div>ChannelDetail</div>;
