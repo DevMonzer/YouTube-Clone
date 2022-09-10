6a8f940497cb1da3bb0cfcdd9322d9fefb220a23
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 18:01:00 +0300

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,6 +6,11 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
   return <div>ChannelDetail</div>;
 };
 
