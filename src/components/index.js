b7590b47e9cc6ec71444e8a924c539272c267820
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 16:51:24 +0300

    add new components

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,6 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Videos } from "./Videos";
+export { default as ChannelCard } from "./ChannelCard";
+export { default as Loader } from "./Loader";
+export { default as VideoCard } from "./VideoCard";
