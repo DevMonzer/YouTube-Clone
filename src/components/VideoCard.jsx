f7fd88221dca60de8f38b06a80b662fd9b74cd85
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 17:01:33 +0300

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
 const VideoCard = ({
   video: {
     id: { videoId },
