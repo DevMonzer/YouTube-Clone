68eeef2d28327f60024c462caa322e7e8beea9e8
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 16:50:07 +0300

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = () => {
-  return <div>Videos</div>;
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
+
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {/* If the item is a video */}
+          {item.id.videoId && <VideoCard video={item} />}
+          {/* If the item is a channel */}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
