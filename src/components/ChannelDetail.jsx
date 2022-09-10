af1a3876c6daa9e8b726521fa0c6a58e1f3e6cf8
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 18:01:51 +0300

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -11,6 +11,22 @@ const ChannelDetail = () => {
 
   const { id } = useParams();
 
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(
+        `search?channelId=${id}&part=snippet%2Cid&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
   return <div>ChannelDetail</div>;
 };
 
