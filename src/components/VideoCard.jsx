d27a3683c9f1e61c0c2729c0307a6a09be54fd42
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-10 17:00:55 +0300

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,8 +3,19 @@ import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
-};
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => (
+  <Card
+    sx={{
+      width: { xs: "100%", sm: "358px", md: "320px" },
+      boxShadow: "none",
+      borderRadius: 0,
+    }}
+  ></Card>
+);
 
 export default VideoCard;
