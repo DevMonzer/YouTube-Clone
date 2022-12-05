f23a80e76cf933a00185b304bcc6101db1d6d3ee
Monzer Mohammad <dev.monzer@gmail.com>
2022-12-05 13:14:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import {
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
-      <Navbar/>
+    <Box sx={{ backgroundColor: "#fff" }}>
+      <Navbar />
       <Routes>
-        <Route exact path="/" element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
