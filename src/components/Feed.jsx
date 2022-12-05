effed7263771af513adb3e025dc3536d96437a94
Monzer Mohammad <dev.monzer@gmail.com>
2022-12-05 13:21:13 +0300

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -52,7 +52,7 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
-        {/* <Videos videos={videos} /> */}
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
