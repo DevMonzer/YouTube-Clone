91f409d82791042e6a552f72fe9469f6ea365090
Monzer Mohammad <dev.monzer@gmail.com>
2022-12-05 13:23:25 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => (
     }}
   >
     <Link to='/' style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt='logo' height={45} />
+      {/* <img src={logo} alt='logo' height={45} /> */}
     </Link>
     <SearchBar />
   </Stack>
