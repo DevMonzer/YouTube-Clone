1462202affb2015bc0fef4e41fa26e47dd121ab0
Monzer Mohammad <dev.monzer@gmail.com>
2022-12-05 13:22:35 +0300

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -30,41 +30,41 @@ const VideoDetail = () => {
   } = videoDetail;
 
   return (
-    <Box minHeight="95vh">
+    <Box minHeight='95vh'>
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
               url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-player"
+              className='react-player'
               controls
             />
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
               {title}
             </Typography>
             <Stack
-              direction="row"
-              justifyContent="space-between"
+              direction='row'
+              justifyContent='space-between'
               sx={{ color: "#fff" }}
               py={1}
               px={2}
             >
               <Link to={`/channel/${channelId}`}>
-                <Typography
+                {/* <Typography
                   variant={{ sm: "subtitle1", md: "h6" }}
-                  color="#fff"
+                  color='#fff'
                 >
                   {channelTitle}
                   <CheckCircleIcon
                     sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
                   />
-                </Typography>
+                </Typography> */}
               </Link>
-              <Stack direction="row" gap="20px" alignItems="center">
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+              <Stack direction='row' gap='20px' alignItems='center'>
+                <Typography variant='body1' sx={{ opacity: 0.7 }}>
                   {parseInt(viewCount).toLocaleString()} views
                 </Typography>
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                <Typography variant='body1' sx={{ opacity: 0.7 }}>
                   {parseInt(likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
@@ -74,10 +74,10 @@ const VideoDetail = () => {
         <Box
           px={2}
           py={{ md: 1, xs: 5 }}
-          justifyContent="center"
-          alignItems="center"
+          justifyContent='center'
+          alignItems='center'
         >
-          <Videos videos={videos} direction="column" />
+          <Videos videos={videos} direction='column' />
         </Box>
       </Stack>
     </Box>
