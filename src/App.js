801d761ba9c84fac99b4f0c0869e360cbf040192
Monzer Mohammed <72494044+DevMonzer@users.noreply.github.com>
2023-01-17 11:09:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
